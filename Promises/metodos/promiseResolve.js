34d57c6a9a6a19e66643472dee8a12dc34776e0c
Roberto Fortes <robertbernd23@gmail.com>
2021-07-17 10:16:40 -0300

    Conceito promise.resolve()

---
+++ Promises/metodos/promiseResolve.js
@@ -0,0 +1,5 @@
+/* O método Promise.resolve(value) retorna um objeto Promise que é resolvido 
+com o valor passado. Se o valor for thenable (ex: tiver um método "then"), 
+a promise retornada irá "seguir" esse thenable, adotando seu estado final; 
+se o valor for uma promise, o objeto será o resultado da chamada 
+Promise.resolve; do contrário a promise será realizada com o valor. */
