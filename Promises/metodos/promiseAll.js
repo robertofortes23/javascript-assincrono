8a96889d8948f4cac071f2a7c713dcad9c5bbd89
Roberto Fortes <robertbernd23@gmail.com>
2021-07-17 10:16:01 -0300

    Conceito promise.all()

---
+++ Promises/metodos/promiseAll.js
@@ -0,0 +1,4 @@
+/* O método Promise.all(iterable) retorna uma única Promise que resolve quando 
+todas as promises no argumento iterável forem resolvidas ou quando o iterável 
+passado como argumento não contém promises. 
+É rejeitado com o motivo da primeira promise que foi rejeitada. */
