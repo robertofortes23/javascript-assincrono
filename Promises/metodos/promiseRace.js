a8b5b1981da4bf2e5ae5a9de73d6a41b8606c77e
Roberto Fortes <robertbernd23@gmail.com>
2021-07-17 10:16:12 -0300

    Conceito promise.race()

---
+++ Promises/metodos/promiseRace.js
@@ -0,0 +1,3 @@
+/* O método Promise.race(iterable) retorna uma promise que resolve ou rejeita 
+assim que uma das promises no iterável resolver ou rejeitar, 
+com o valor ou razão daquela promise. */
