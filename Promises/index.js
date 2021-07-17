a56feec4dd411f229b18dfddfe80829aa22034f6
Roberto Fortes <robertbernd23@gmail.com>
2021-07-17 10:15:47 -0300

    Conceito de promise

---
+++ Promises/index.js
@@ -0,0 +1,8 @@
+/*Promise é um objeto usado para processamento assíncrono. 
+Um Promise (de "promessa") representa um valor que pode estar disponível agora, 
+no futuro ou nunca.*/
+
+/*Isto permite que métodos assíncronos retornem valores como métodos síncronos: 
+ao invés do valor final, o método assíncrono retorna uma promessa ao valor 
+em algum momento no futuro.*/
+
