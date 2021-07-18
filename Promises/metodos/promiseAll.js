6a417e697444ad499e77ced9c2c7710eaa31f692
Roberto Gomes Fortes <52727243+robertofortes23@users.noreply.github.com>
2021-07-18 17:53:15 -0300

    Invocando métodos em promisse.all()

--- Promises/metodos/promiseAll.js
+++ Promises/metodos/promiseAll.js
@@ -12,3 +12,9 @@ Promise.all([ pA, pB, pC, pD, pE])
     .catch(err => {
         console.warn('Failed: ', err);
     });
+
+let pA = new Promise((resolve, reject) => time('A', 500, resolve));
+let pB = new Promise((resolve, reject) => time('B', 300, resolve));
+let pC = new Promise((resolve, reject) => time('C', 800, resolve));
+let pD = new Promise((resolve, reject) => time('D', 400, resolve));
+let pE = new Promise((resolve, reject) => time('E', 1000, resolve));
