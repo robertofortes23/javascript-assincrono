b1be146d46528cb76b800009f63c2d6c678707f0
Roberto Gomes Fortes <52727243+robertofortes23@users.noreply.github.com>
2021-07-18 17:52:40 -0300

    Criando promise e time

--- Promises/metodos/promiseAll.js
+++ Promises/metodos/promiseAll.js
@@ -2,3 +2,13 @@
 todas as promises no argumento iterável forem resolvidas ou quando o iterável 
 passado como argumento não contém promises. 
 É rejeitado com o motivo da primeira promise que foi rejeitada. */
+
+let time = (x, y, fn) => setTimeout(() => { fn(x) }, y);
+
+Promise.all([ pA, pB, pC, pD, pE])
+    .then(result => {
+        console.log('Resultado da promise.all: ', result);
+    })
+    .catch(err => {
+        console.warn('Failed: ', err);
+    });
