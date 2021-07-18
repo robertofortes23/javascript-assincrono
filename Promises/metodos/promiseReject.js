f56dde84f66a44bf15f2b92bf98c85ea7ab97087
Roberto Gomes Fortes <52727243+robertofortes23@users.noreply.github.com>
2021-07-18 20:48:51 -0300

    Lógica método promise.reject()

--- Promises/metodos/promiseReject.js
+++ Promises/metodos/promiseReject.js
@@ -1,3 +1,15 @@
 /* O método Promise.reject(motivo) retorna um objeto Promise que é rejeitada com 
-um dado motivo. */
+um dado motivo. Usado para fins de debug e captura de erros seletiva, é útil que 
+o motivo seja uma instanceof Error.*/
 
+Promise.reject("Testando reject estático").then(function(motivo) {
+  // não executado
+}, function(motivo) {
+  console.log(motivo); // "Testando reject estático"
+});
+
+Promise.reject(new Error("falha")).then(function(erro) {
+  // não executado
+}, function(erro) {
+  console.log(erro); // Stacktrace
+});
