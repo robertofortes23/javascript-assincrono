34c14452439cc2293766893c6c39bef2ccf2b20d
Roberto Gomes Fortes <52727243+robertofortes23@users.noreply.github.com>
2021-07-19 21:11:31 -0300

    Exemplo usando primise.resolve()

--- Promises/metodos/promiseResolve.js
+++ Promises/metodos/promiseResolve.js
@@ -3,3 +3,9 @@ com o valor passado. Se o valor for thenable (ex: tiver um método "then"),
 a promise retornada irá "seguir" esse thenable, adotando seu estado final; 
 se o valor for uma promise, o objeto será o resultado da chamada 
 Promise.resolve; do contrário a promise será realizada com o valor. */
+
+Promise.resolve("Success").then(function(value) {
+  console.log(value); // "Success"
+}, function(value) {
+  // not called
+});
