2d8bf9267825f29f2c74e61c780c6e6a3e8f26e5
Roberto Fortes <robertbernd23@gmail.com>
2021-07-20 20:31:29 -0300

    Capturando evento de click

--- XMLHttpRequest/assets/js/main.js
+++ XMLHttpRequest/assets/js/main.js
@@ -1,7 +1,7 @@
 const request = obj => {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
-    xhr.open(obj.method, obj.url, true);
+    xhr.open(obj.method, obj.url, true); //true pra async
     xhr.send();
 
     xhr.addEventListener('load', () => {
@@ -13,3 +13,13 @@ const request = obj => {
     });
   });
 };
+
+document.addEventListener('click', e => {
+  const el = e.target;
+  const tag = el.tagName.toLowerCase();
+
+  if (tag === 'a') {
+    e.preventDefault();
+    carregaPagina(el);
+  }
+});
