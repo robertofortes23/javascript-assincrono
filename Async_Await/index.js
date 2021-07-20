de279d67f4f2c06b6acf2e7fccc6d4a75d348d47
Roberto Fortes <robertbernd23@gmail.com>
2021-07-20 20:09:48 -0300

    Definição de await

--- Async_Await/index.js
+++ Async_Await/index.js
@@ -1,3 +1,6 @@
 /* A palavra “async” antes de uma função significa uma coisa simples: uma função 
 sempre retorna uma promessa. Outros valores são incluídos em uma promessa 
-resolvida automaticamente. */
+resolvida automaticamente. 
+
+A palavra-chave "await" faz o JavaScript esperar até que a promessa seja 
+estabelecida e retorne seu resultado. */
