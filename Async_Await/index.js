213f4015e265fc70cba421376af943222413de5d
Roberto Fortes <robertbernd23@gmail.com>
2021-07-20 20:13:57 -0300

    Estrutura async

--- Async_Await/index.js
+++ Async_Await/index.js
@@ -1,6 +1,9 @@
 /* A palavra “async” antes de uma função significa uma coisa simples: uma função 
 sempre retorna uma promessa. Outros valores são incluídos em uma promessa 
-resolvida automaticamente. 
+resolvida automaticamente. */
 
-A palavra-chave "await" faz o JavaScript esperar até que a promessa seja 
-estabelecida e retorne seu resultado. */
+async function funcao() {
+    return 1;
+}
+
+funcao().then(alert); // 1
