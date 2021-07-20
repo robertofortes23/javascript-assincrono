d15cc94f06d04753e0c582bc1258efa83409f739
Roberto Fortes <robertbernd23@gmail.com>
2021-07-20 20:14:26 -0300

    Chamada await, explicação

--- Async_Await/index.js
+++ Async_Await/index.js
@@ -7,3 +7,14 @@ async function funcao() {
 }
 
 funcao().then(alert); // 1
+
+/* A palavra-chave "await" faz o JavaScript esperar até que a promessa seja 
+estabelecida e retorne seu resultado. */
+
+// funciona apenas dentro de funções assíncronas
+let value = await promise;
+
+/* await literalmente suspende a execução da função até que a promessa seja 
+concluída e, em seguida, a retoma com o resultado da promessa. 
+Isso não custa nenhum recurso da CPU, porque o mecanismo JavaScript pode fazer 
+outras tarefas nesse ínterim: executar outros scripts, manipular eventos, etc. */
