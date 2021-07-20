e0b67798cf84cf77e1c8416b594de7bb35b7f81b
Roberto Fortes <robertbernd23@gmail.com>
2021-07-20 20:08:04 -0300

    Definição de async

---
+++ Async_Await/index.js
@@ -0,0 +1,3 @@
+/* A palavra “async” antes de uma função significa uma coisa simples: uma função 
+sempre retorna uma promessa. Outros valores são incluídos em uma promessa 
+resolvida automaticamente. */
