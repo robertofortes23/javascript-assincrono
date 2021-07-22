d60c0dd8c089d028ad7b9a325d68e3c11bbbad56
Roberto Fortes <robertbernd23@gmail.com>
2021-07-22 12:41:46 -0300

    Adicionando requisição com axios

--- Fetch_Axios/assets/js/main.js
+++ Fetch_Axios/assets/js/main.js
@@ -2,3 +2,5 @@ fetch('pessoas.json')
   .then(resposta => resposta.json())
   .then(json => carregaElementosNaPagina(json));
 
+// axios('pessoas.json')
+//   .then(resposta => carregaElementosNaPagina(resposta.data));
